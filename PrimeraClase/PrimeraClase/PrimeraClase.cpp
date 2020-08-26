27fe5f4c394f4afdeb2ba8a96b6107f81a47a77b
italo alex kancha vilca <kanchavilcai@gmail.com>
2020-08-26 09:16:50 -0500

    hola

---
+++ PrimeraClase/PrimeraClase/PrimeraClase.cpp
@@ -0,0 +1,11 @@
+// PrimeraClase.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "HOLA" << endl;
+	return 0;
+}
